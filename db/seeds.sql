3cbff1014dd942388dd5874c3e1dde1fa28519d1
Matthew Leonard <leonard.matthewk@gmail.com>
2020-05-09 16:35:13 -0400

    updating seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into Burgers (name, eaten) values ('Cheese Burger', false);
+insert into Burgers (name, eaten) values ('Hamburger', false);
+insert into Burgers (name, eaten) values ('Bacon Cheese Burger', false);
