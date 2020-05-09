cc5e089670868762e782d6f5239ed9816519ff60
Matthew Leonard <leonard.matthewk@gmail.com>
2020-05-09 16:24:33 -0400

    updated backend to correct issues

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,61 @@
+$(function() {
+    $(".change-eat").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEat = $(this).data("neweat");
+        console.log(newEat, id)
+      var newEatState = {
+        eaten: newEat
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatState
+      }).then(
+        function() {
+          console.log("changed eat to", newEat);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+        console.log($("#bur").val().trim()) 
+        console.log($("[name=eaten]:checked").val().trim())
+      var newBurger = {
+        name: $("#bur").val().trim(),
+        eaten: $("[name=eaten]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
